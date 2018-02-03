043171bd09e7229b22a60592034ee5d492c203ab
Navon Francis <navonfrancis@gmail.com>
2018-02-03 01:11:20 -0500

    added service worker for progressive app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
+import { Router, browserHistory, Route, Link } from 'react-router';
 import logo from './logo.svg';
 import './App.css';
+
+const Page = ({ title }) => (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>{title}</h2>
+      </div>
+      <p className="App-intro">
+        This is the {title} page.
+      </p>
+      <p>
+        <Link to="/">Home</Link>
+      </p>
+      <p>
+        <Link to="/about">About</Link>
+      </p>
+      <p>
+        <Link to="/settings">Settings</Link>
+      </p>
+    </div>
+);
+
+const Home = (props) => (
+  <Page title="Home"/>
+);
+
+const About = (props) => (
+  <Page title="About"/>
+);
+
+const Settings = (props) => (
+  <Page title="Settings"/>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router history={browserHistory}>
+        <Route path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/settings" component={Settings}/>
+      </Router>
     );
   }
 }
